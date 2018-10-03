f294ebf732d26d8877717c8d3462e6e9bf6b6ea2
Daniel <dtkahn@gmail.com>
2018-10-03 14:25:32 -0400

    initial setup of index.js and StorePicker component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class StorePicker extends Component {
+  render() {
+    return (
+      <form className="store-selector">
+        <h2>Enter A Store</h2>
+        <input type="text" required placeholder="store" />
+        <button type="submit">Visit Store</button>
+      
+      </form>
+    )
+  }
+}
+
+export default StorePicker;
