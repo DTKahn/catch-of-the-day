edb05786a7917fb8b0bb3f4a4ad83caab1de0412
Daniel <dtkahn@gmail.com>
2018-10-04 14:59:03 -0400

    created AddFishForm component and imported to Inventory component

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
+import AddFishForm from './AddFishForm';
+
 
 class Inventory extends Component {
   render() {
     return (
       <div className="inventory">
-        Inventory!!!
+        <h2>Inventory</h2>
+        <AddFishForm />
       </div>
     );
-
   }
 }
 
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,12 +2,29 @@ import React, { Component } from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends Component {
+  
+  myInput = React.createRef();
+
+  
+  goToStore = event => {
+    // Stop form from submitting
+    event.preventDefault();
+    
+    // Get text from input
+    const storeName = this.myInput.value.value;
+    
+    // Change page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Enter A Store</h2>
+        
         <input 
           type="text" 
+          ref={this.myInput}
           required 
           placeholder="store name" 
           defaultValue={getFunName()}
