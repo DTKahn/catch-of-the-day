18f1f9d47ad130cf73db579bcedd39fae9526f71
Daniel <dtkahn@gmail.com>
2018-10-05 11:03:23 -0400

    App added addToOrder method and a map for displaying the fish. Fish added an is available check and modify look of the add to order button if not available

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -33,6 +33,20 @@ class App extends Component {
     });
   };
 
+  addToOrder = key => {
+    
+    // Take a copy of state
+    const order = {...this.state.order};
+
+    // Add to the order or update the number in order if already in order
+    order[key] = order[key] + 1 || 1;
+
+    // Call setState to update the state object
+    this.setState({
+      order: order
+    })
+  };
+
   render () {
     return (
       <div className="catch-of-the-day">
@@ -40,7 +54,14 @@ class App extends Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+            {Object.keys(this.state.fishes).map(key => (
+              <Fish 
+                key={key} 
+                index={key}
+                details={this.state.fishes[key]} 
+                addToOrder={this.addToOrder}
+              />
+            ))}
           </ul>
         </div>
         
