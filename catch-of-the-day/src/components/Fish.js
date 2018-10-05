4a7c28d55395af8ff2053a4b084d86bccb389cb4
Daniel <dtkahn@gmail.com>
2018-10-05 11:04:40 -0400

    accidentally unstaged the changes for the Fish component. Commiting and pushing

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -10,13 +10,17 @@ class Fish extends Component {
     const desc = this.props.details.desc;
     const status = this.props.details.status;
 
+    const isAvailable = status === 'available';
+    
     return (
       <li className="menu-fish">
         <img src={image} alt={name}/>
         <h3 className="fish-name">{name}</h3>
         <span className="price">{formatPrice(price)}</span>
         <p>{desc}</p>
-        <button>Add to Cart</button>
+        <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
+          {isAvailable ? 'Add to Order' : 'Sold Out!'}
+        </button>
       </li>
     );
 
